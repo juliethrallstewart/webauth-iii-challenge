9ff77fd621ef99ac3db546e0c72cc1c095b9f1eb
Julie Thrall <juliethrallstewart@gmail.com>
2019-09-18 16:12:03 -0700

    completed MVP

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: 'you shall not pass' });
+
+      } else {
+        res.user = { username: decodedToken.username}
+          next()
+        }
+        
+      })
+    } else {
+      res.status(400).json({message: 'no credentials provided'})
+    }
+}
